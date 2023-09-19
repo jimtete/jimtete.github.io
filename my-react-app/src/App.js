510bfba1ed79a525306bdf8fd9dc365624e8792a
Dimitris Tetepoulidis <tetepoulidis@ieee.org>
2023-09-19 13:31:02 +0300

    initialized main files

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,11 +1,26 @@
+import React from 'react';
 import './App.css';
+import Navbar from './components/Navbar/Index';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages';
+import About from './pages/about';
+import Events from './pages/events';
+import AnnualReport from './pages/annual';
+import Blogs from './pages/blogs';
 
 function App() {
-  return (
-    <div className="App">
-      <p>Test</p>
-    </div>
-  );
+    return (
+        <Router>
+            <Navbar />
+            <Routes>
+                <Route path='/' exact component={Home} />
+                <Route path='/about' component={About} />
+                <Route path='/events' component={Events} />
+                <Route path='/annual' component={AnnualReport} />
+                <Route path='/blogs' component={Blogs} />
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
