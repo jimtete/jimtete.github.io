317a458114adc4880d81c77147518962667e40d3
Dimitris Tetepoulidis <tetepoulidis@ieee.org>
2023-10-24 14:35:40 +0300

    tailwind installation

--- my-react-app/src/components/Navbar/Item.js
+++ my-react-app/src/components/Navbar/Item.js
@@ -1,9 +1,10 @@
 import React from "react";
 import classes from "./NavbarElement.module.css";
+import '../../index.css';
 
 const Item = ({ itemName }) => {
     return (
-        <div className={classes.element_bg}>
+        <div class='text-xl' className={classes.element_bg}>
             {itemName}
         </div>
     )
